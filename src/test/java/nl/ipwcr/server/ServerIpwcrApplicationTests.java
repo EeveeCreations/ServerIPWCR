6bba7c4f5fb27f9b1e2bceb5ea89c54554cd0aca
AntiEevee <eefjekarremans@hotmail.nl>
2022-01-21 02:53:21 +0100

    Add TestofTrue

--- src/test/java/nl/ipwcr/server/ServerIpwcrApplicationTests.java
+++ src/test/java/nl/ipwcr/server/ServerIpwcrApplicationTests.java
@@ -1,14 +1,7 @@
 package nl.ipwcr.server;
 
-import nl.ipwcr.server.filter.AuthenticationFilter;
-import nl.ipwcr.server.filter.AuthorisationFilter;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.security.core.Authentication;
-
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import java.net.http.HttpResponse;
 
 @SpringBootTest
 class ServerIpwcrApplicationTests {
